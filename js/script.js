5980aa84265435de27b9e441e15dc3eafbc485be
Rafal Swieton <rafal@eskrzynka.pl>
2018-02-11 01:44:43 +0100

    Change script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 // script.js
 
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.',
+	dinosaur = 'triceratops',
+	dinosaurUpperCased = dinosaur.toUpperCase(),
+	textDinosaur = text.replace('Velociraptor', dinosaurUpperCased),
+	partOftextDinosaur = textDinosaur.slice(0,72);
+
+console.log(textDinosaur.length/2);
+console.log(partOftextDinosaur);
+
+
